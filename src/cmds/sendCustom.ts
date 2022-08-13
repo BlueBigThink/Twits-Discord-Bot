0fc5ec35cf1182195b1222a5d27917e53bd81e45
BlueBigThink <bluebigthink@gmail.com>
2022-08-13 22:58:03 +0530

    custom message fixed

--- src/cmds/sendCustom.ts
+++ src/cmds/sendCustom.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Client, Message } from "discord.js-selfbot-v13";
-import { browser, Config, discord } from "..";
+import { browser, Config, discord, log } from "..";
 import { Command } from "../types/commands";
 import discordLogin from "../utils/discordLogin";
 import getMessageImage from "../utils/getMessageImage";
@@ -12,29 +12,30 @@ const cmd: Command = {
   triggers: ["send", "sendCustom"],
   exec: async function (client: Client, message: Message, args: string[]) {
     if (args.length < 2) return message.reply("Message cannot be empty");
-
+    args.splice(0, 1);
     let image: Buffer;
     const { attachments } = message;
-
-    switch (true) {
-      case attachments.size > 0: {
-        const response = await axios(
-          String(attachments.toJSON()[0].attachment),
-          {
-            responseType: "arraybuffer",
-          }
-        );
-        image = Buffer.from(response.data, "base64");
-        break;
-      }
-
-      default:
-        image = await getMessageImage(discord, message);
+    const content = args.toString().replaceAll(",", " ");
+    log("Processing custom message");
+    if (attachments.size) {
+      const response = await axios(String(attachments.toJSON()[0].attachment), {
+        responseType: "arraybuffer",
+      });
+      image = Buffer.from(response.data, "base64");
+    } else {
+      image = await getMessageImage(discord, message);
     }
-    await postStocktwits(message.content, image).catch((err) =>
-      console.log(err)
-    );
-    await postTwitter(message.content, image).catch((err) => console.log(err));
+
+    await postStocktwits(content, image)
+      .then(() => log(`Posted custom message on stocktwits \n${content}\n`))
+      .catch((err) =>
+        log(`Error while posting custom message on stocktwits\n${err}\n`)
+      );
+    await postTwitter(content, image)
+      .then(() => log(`Posted custom message on twitter \n${content}\n`))
+      .catch((err) =>
+        log(`Error while posting custom message on twitter\n${err}\n`)
+      );
   },
 };
 
