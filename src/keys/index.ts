f26e53a05a1f8bacdbcfea8a0db94b700da86e73
BlueBigThink <bluebigthink@gmail.com>
2022-12-24 00:34:15 +0500

    Add util for twitter

--- src/keys/index.ts
+++ src/keys/index.ts
@@ -20,6 +20,15 @@ export const sentryDsn = isProd
 export const stocktwitsApiKey = isProd
   ? process.env.STOCKTWITS_API_KEY!
   : process.env.STOCKTWITS_API_KEY_DEV!;
-export const twitterBearerToken = isProd
-  ? process.env.TWITTER_BEARER_TOKEN!
-  : process.env.TWITTER_BEARER_TOKEN_DEV!;
+export const twitterConsumerKey = isProd
+  ? process.env.TWITTER_CONSUMER_KEY!
+  : process.env.TWITTER_CONSUMER_KEY_DEV!;
+export const twitterConsumerSecret = isProd
+  ? process.env.TWITTER_CONSUMER_SECRET!
+  : process.env.TWITTER_CONSUMER_SECRET_DEV!;
+export const twitterAccessToken = isProd
+  ? process.env.TWITTER_ACCESS_TOKEN!
+  : process.env.TWITTER_ACCESS_TOKEN_DEV!;
+export const twitterAccessTokenSecret = isProd
+  ? process.env.TWITTER_ACCESS_TOKEN_SECRET!
+  : process.env.TWITTER_ACCESS_TOKEN_SECRET_DEV!;
--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -1,7 +1,35 @@
 // IMPORTS
-import { twitterBearerToken } from '@keys';
+import {
+  twitterAccessToken,
+  twitterAccessTokenSecret,
+  twitterConsumerKey,
+  twitterConsumerSecret,
+} from '@keys';
 import { handleError } from '@utils';
+import { TwitterClient } from 'twitter-api-client';
 
 // INIT
+const twitterClient = new TwitterClient({
+  apiKey: twitterConsumerKey,
+  apiSecret: twitterConsumerSecret,
+  accessToken: twitterAccessToken,
+  accessTokenSecret: twitterAccessTokenSecret,
+});
 
 // TWITTER UTILS
+export const postToTwitter = async (content: string, image: Buffer) => {
+  try {
+    const uploaded = await twitterClient.media.mediaUpload({
+      media_data: image.toString('base64'),
+      media_category: 'tweet_image',
+    });
+
+    return await twitterClient.tweetsV2.createTweet({
+      media: { media_ids: [uploaded.media_id_string] },
+      text: content,
+    });
+  } catch (e) {
+    handleError(e);
+    return e;
+  }
+};
